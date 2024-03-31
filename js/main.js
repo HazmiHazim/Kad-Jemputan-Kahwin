3184873cecd7ee6ece08af194515c20072d9c83e
HazmiHazim <hazimnetto01@gmail.com>
2024-03-31 18:14:28 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,133 @@
+// Toggle background active
+const slideNavigator = name => {
+    let slides = document.querySelectorAll('.bg-slide');
+    slides.forEach(slide => {
+        slide.classList.remove('active');
+        if (slide.classList.contains(name)) {
+            slide.classList.add('active');
+        }
+    });
+};
+
+
+// Switch background
+window.addEventListener('load', () => {
+    const slideBtnList = document.querySelectorAll('.slide-btn');
+    slideBtnList.forEach(btn => {
+        btn.addEventListener('click', function (e) {
+            e.preventDefault();
+            slideBtnList.forEach(el => {
+                el.classList.remove('active');
+            });
+            this.classList.add('active');
+            slideNavigator(this.getAttribute('data-target'));
+        });
+    });
+});
+
+
+// Activate sections
+const sectionNavigator = name => {
+    let sections = document.querySelectorAll('section');
+    let header = document.querySelector('header');
+    sections.forEach(section => {
+        section.classList.remove('section-show');
+        if (section.classList.contains(name)) {
+            section.classList.add('section-show');
+            header.classList.add('active');
+        }
+    });
+};
+
+
+// Navigation to sections
+window.addEventListener('load', () => {
+    const navList = document.querySelectorAll('.nav-btn');
+    navList.forEach(nav => {
+        nav.addEventListener('click', function (e) {
+            e.preventDefault();
+            navList.forEach(el => {
+                el.classList.remove('active');
+            });
+            this.classList.add('active');
+            sectionNavigator(this.getAttribute('data-target'));
+            screen.width < 768 && toggleMenu();
+        });
+    });
+});
+
+
+// Reset header to initial state
+const resetHeader = () => {
+    let header = document.querySelector('header');
+    header.classList.remove('active');
+};
+
+
+// Initial navigation
+const initNavigation = () => {
+    const navList = document.querySelectorAll('.nav-btn');
+    navList.forEach(el => {
+        el.classList.remove('active');
+        if (el.getAttribute('data-target') === 'about') {
+            el.classList.add('active');
+        }
+    });
+    sectionNavigator('about');
+};
+
+
+// Toggle menu
+const toggleMenu = () => {
+    const menu = document.querySelector('.menu');
+    const navMobile = document.querySelector('.nav-mobile');
+    menu.classList.toggle('active');
+    navMobile.classList.toggle('active');
+};
+
+
+
+/** Countdown Timer */
+(function () {
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+    //I'm adding this section so I don't have to keep updating this pen every year :-)
+    //remove this if you don't need it
+    let today = new Date(),
+        dd = String(today.getDate()).padStart(2, "0"),
+        mm = String(today.getMonth() + 1).padStart(2, "0"),
+        yyyy = today.getFullYear(),
+        nextYear = yyyy + 1,
+        dayMonth = "06/14/",
+        birthday = dayMonth + yyyy;
+
+    today = mm + "/" + dd + "/" + yyyy;
+    if (today > birthday) {
+        birthday = dayMonth + nextYear;
+    }
+    //end
+
+    const countDown = new Date(birthday).getTime(),
+        x = setInterval(function () {
+
+            const now = new Date().getTime(),
+                distance = countDown - now;
+
+            document.getElementById("days").innerText = Math.floor(distance / (day)),
+                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+            //do something later when date is reached
+            if (distance < 0) {
+                document.getElementById("headline").innerText = "It's my birthday!";
+                document.getElementById("countdown").style.display = "none";
+                document.getElementById("content").style.display = "block";
+                clearInterval(x);
+            }
+            //seconds
+        }, 0)
+}());
