70dcc0dc125192469da287dc4dfecf6100250c42
HazmiHazim <hazimnetto01@gmail.com>
2024-04-29 09:12:06 +0800

    Update Ucapan Button

--- js/main.js
+++ js/main.js
@@ -221,7 +221,7 @@ function reveal() {
     for (var i = 0; i < reveals.length; i++) {
         var windowHeight = window.innerHeight;
         var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
+        var elementVisible = 10;
 
         if (elementTop < windowHeight - elementVisible) {
             reveals[i].classList.add("active");
